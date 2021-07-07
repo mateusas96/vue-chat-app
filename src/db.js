b52090004c693c04102fab5ec2d3f86b1d362e4d
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-07-07 19:23:29 +0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId
+};
+
+const db = firebase.initializeApp(config);
+export default db;
